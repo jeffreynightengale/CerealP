64d1d8cf4b89ec6cf7af4d1575d92a80ed59e96e
Jeff <Jeff@DESKTOP-DFBAMBV>
2021-08-02 15:09:25 -0500

    Started

---
+++ Cereal/Cereal/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+namespace Cereal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = File.ReadAllLines("Cereal_Data.txt");
+            string findingCups = lines[3];
+            string findingCalories = lines[2];
+
+            for (int i = 1; i < findingCups.Length; i++)
+            {
+                double pieces = findingCups[i];
+                if (pieces <= 1)
+                {
+                    Console.WriteLine(lines);
+                }
+            }
+        }
+    }
+}
